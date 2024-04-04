011b2dd2f88621f1f59742a8a63e1f1f06943dc8
paroom6 <opwqer3729@gmail.com>
2024-04-04 17:20:44 +0900

    feat: myPage, teamRegistPage 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { getPrincipalRequest } from './apis/api/principal';
+import { useQuery } from 'react-query';
+import MyPage from './pages/MyPage/MyPage';
+import TeamCreatePage from './pages/TeamCreatePage/TeamCreatePage';
 
 function App() {
+  const principalQuery = useQuery(["principalQuery"], getPrincipalRequest,{//focus 변경정로도
+    retry: 0,
+    refetchOnWindowFocus: false,
+    onSuccess: response => {
+        console.log("onSuccess");
+        
+    },
+    onError: error => {
+        console.log("오류");
+        console.log(error);
+    }
+});
+
   return (
-    <>
-    </>
+    <Routes>
+      <Route path="/account/mypage" element={ <MyPage />}/>
+      <Route path="/account/create/team" element={ <TeamCreatePage />}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/TeamInfoPage/TeamInfoPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TeamInfoPage(props) {
+    return (
+        <div>
+            팀 소개
+        </div>
+    );
+}
+
+export default TeamInfoPage;
