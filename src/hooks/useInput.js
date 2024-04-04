c319e7bcb6fea8c72a8e5271b527f8fb08da8a82
snowypark <partsnowy@gmail.com>
2024-04-04 17:55:37 +0900

    merge

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { REGEX } from "../constants/regex";
 
 export const useInput = (property) => {
-    const [ value, setValue ] = useState("");
-    const [ message, setMessage ] = useState(null);
+    
+    const [value, setValue] = useState("");
+    const [message, setMessage] = useState(null);
+
+    useEffect(() => {
         const regexEntries = Object.entries(REGEX);
-        for(let [ k, v ] of regexEntries) {
-            if(property === k) {
-                if(v.regexr.test(value)) {
-                    setMessage(() => {
-                        return {
-                            type: "success",
-                            text: ""
-                        }
-                    })
-                }else {
-                    setMessage(() => {
-                        return {
-                            type: "error",
-                        }
-                    })
+        for (let [k, v] of regexEntries) {
+            if (property === k) {
+                if (v.regexr.test(value)) {
+                    setMessage({
+                        type: "success",
+                        text: ""
+                    });
+                } else {
+                    setMessage({
+                        type: "error",
+                    });
                 }
             }
         }
-    }, [value])
-    const handleOnChange = (e) => {
-        setValue(() => e.target.value);
-    }
+    }, [value, property]);
 
-    return [ value, handleOnChange, message, setValue, setMessage ];
-}
+    const handleOnChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    return [value, handleOnChange, message, setValue, setMessage];
+};
