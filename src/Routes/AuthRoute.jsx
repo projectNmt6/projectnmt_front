b305a9cc896054165a68f434664ce8fe0d89bfdb
snowypark <partsnowy@gmail.com>
2024-04-05 09:26:01 +0900

    start

--- src/Routes/AuthRoute.jsx
+++ src/Routes/AuthRoute.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import SignInPage from '../pages/SignInPage/SignInPage';
 import { Route, Routes } from 'react-router-dom';
-import SignUpPage from '../pages/SignUpPage/SignUpPage';
-import { Route, Routes } from "react-router-dom";
 import DonationPageboard from "../pages/DonationPageBoard/DonationPageboard";
 import HomePage from "../pages/HomePage";
 import MainPage from "../pages/MainPage/MainPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage"
+
 
 function AuthRoute(props) {
     return (
         <Routes>
-            <Route path='/signin' element={ <SignInPage /> }/>
             <Route path='/signup' element={ <SignUpPage /> }/>
             <Route path="/" element={<HomePage />} />
             <Route path="/main" element={<MainPage />} />
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react"
 import { REGEX } from "../constants/regex";
 
+
 export const useInput = (property) => {
     const [ value, setValue ] = useState("");
     const [ message, setMessage ] = useState(null);
+
+    useEffect(() => {
+        if(!value) {
+            setMessage(() => null);
+            return;
+        }
+
         const regexEntries = Object.entries(REGEX);
         for(let [ k, v ] of regexEntries) {
             if(property === k) {
@@ -18,12 +26,14 @@ export const useInput = (property) => {
                     setMessage(() => {
                         return {
                             type: "error",
+                            text: v.text
                         }
                     })
                 }
             }
         }
     }, [value])
+
     const handleOnChange = (e) => {
         setValue(() => e.target.value);
     }
