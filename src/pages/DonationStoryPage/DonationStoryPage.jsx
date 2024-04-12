af42f65091574764a32b7145d06a97883aa06de7
snowypark <partsnowy@gmail.com>
2024-04-12 09:46:10 +0900

    .

--- src/pages/DonationStoryPage/DonationStoryPage.jsx
+++ src/pages/DonationStoryPage/DonationStoryPage.jsx
@@ -2,14 +2,23 @@
 import React, { useEffect, useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 import {Link, useLocation, useParams } from 'react-router-dom';
-import { getDonationStoryRequest, updatePageRequest } from '../../apis/api/DonationAPI';
+import { commentReqest, commentResponse, deleteDonationPage, getDonationStoryRequest, updatePageRequest } from '../../apis/api/DonationAPI';
 import DOMPurify from 'dompurify';
+/** @jsxImportSource @emotion/react */
+import * as s from "./style";
+import axios from 'axios';
+import CommentSection from '../../pages/DonationStoryPage/CommentSection'; 
 
 function DonationStoryPage() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const donationPageId = queryParams.get('page'); 
     const[donationPage, setDonationPage] = useState({});
+    const [ commentList, setCommentList ] = useState([]);
+    const donationCommentId = queryParams.get('commentId')
+
+    const [comment, setComment ] = useState("");
+
     const getDonationStoryQuery = useQuery(
         ["getDonationPageQuery", donationPageId], 
         async () => {
@@ -22,15 +31,55 @@ function DonationStoryPage() {
             }
         }
     );
+
+    
+    useEffect(() => {
+        axios.get(`http://localhost:8080/comment/getcomment/${donationPageId}`)
+            .then(response => setCommentList(response.data))
+            .catch(console.error);
+    }, [donationPageId]);
+    
+
     const { storyContent, storyTitle, mainImgUrl, createDate, endDate } = donationPage || {};
 
     const safeHTML = DOMPurify.sanitize(donationPage.storyContent);
+    
+    const deleteMutationButton = useMutation({
+        mutationKey: "deleteMutationButton",
+        mutationFn: deleteDonationPage,
+        onSuccess: response => {
+            alert("삭제완료")
+            window.location.replace("/main");
+        }
+    })
 
-    console.log(donationPage);
-
-    const deleteButton = () => {
-        
+    const handleDeleteButtonClick = () => {
+        deleteMutationButton.mutate({ donationPageId: donationPageId });
     }
+
+
+    const handleCommentChange = (e) => {
+        const value = e.target.value;
+        setComment(value);
+    }
+
+    const handleCommentSubmit = () => {
+
+        axios.post("http://localhost:8080/comment/upload", {
+            donationCommentId: null,
+            commentText : comment,
+            donationPageId: donationPageId,
+            userId: null
+        })
+        .then(response => {
+            alert("전송 완료")
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+ 
 
     useEffect(() => {
         const fetchData = async () => {
@@ -53,7 +102,7 @@ function DonationStoryPage() {
             
                 <div>
                 <Link to={`/main/donation/update?page=${donationPageId}`}>수정하기</Link>                
-                <button onClick={deleteButton} >삭제하기</button>
+                <button onClick={handleDeleteButtonClick} >삭제하기</button>
             </div>
 
 
@@ -68,6 +117,31 @@ function DonationStoryPage() {
                     <p>기부 시작일: {donationPage.createDate}</p>
                     <p>기부 종료일: {donationPage.endDate}</p>
                     <div dangerouslySetInnerHTML={{ __html: safeHTML }} />
+            </div>
+
+            <div css={s.boxbox1}>
+                분리 공간
+            </div>
+
+                <h3>덧글</h3>
+            <div css={s.commentBox}>
+
+                <div>
+                <CommentSection donationPageId={donationPageId}>
+                    
+                </CommentSection>
+                </div>
+
+                {/* <div>
+                    <input 
+                        type="text" 
+                        value={comment}
+                        onChange={handleCommentChange}
+                    />
+                    <p>{commentList.commentText}</p>
+                    <button onClick={handleCommentSubmit}>덧글입력</button>
+                </div> */}
+
             </div>
 
         </>
