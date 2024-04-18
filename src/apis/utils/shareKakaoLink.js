9482270df576c587d6241b88aeab265c6a603322
snowypark <partsnowy@gmail.com>
2024-04-18 09:37:45 +0900

    .

---
+++ src/apis/utils/shareKakaoLink.js
@@ -0,0 +1,31 @@
+export const shareKakao = (route, title) => { // url이 id값에 따라 변경되기 때문에 route를 인자값으로 받아줌
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init(process.env.REACT_APP_SHARE_KAKAO_LINK_KEY); 
+        // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+      }
+  
+      kakao.Link.sendDefault({
+        objectType: "feed",
+        content: {
+          title: title, // 인자값으로 받은 title
+          description: "설명", // 인자값으로 받은 title
+          imageUrl: "이미지 url",
+          link: {
+            mobileWebUrl: route, // 인자값으로 받은 route(uri 형태)
+            webUrl: route
+          }
+        },
+        buttons: [
+          {
+            title: "title",
+            link: {
+              mobileWebUrl: "https://www.naver.com",
+              webUrl: "https://www.naver.com"
+            }
+          }
+        ]
+      });
+    }
+  };
