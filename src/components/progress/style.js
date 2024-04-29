ab2c57770eed4762f1c53a44658910a9363d7ebc
Byeonjichan <jaekyeon14@naver.com>
2024-04-29 09:09:21 +0900

    feat: font 설정

--- src/components/progress/style.js
+++ src/components/progress/style.js
@@ -6,8 +6,13 @@ export const header = css`
     justify-content: space-between;
     align-items: center;
     width: 100%;
+    @font-face {
+    font-family: 'omyu_pretty';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-01@1.0/omyu_pretty.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
 `;
-
 
 export const progressbar = css`
     width: 100%;
@@ -15,11 +20,13 @@ export const progressbar = css`
     background-color: #dbecf7;
     border-radius: 4px;
     overflow: hidden;
+    font-family: 'omyu_pretty';
     font-size: 15px;
     margin-bottom: 5px;
 `;
 export const progress = css`
     height: 100%;
+    font-family: 'omyu_pretty';
     background-color: #007bff;
     transition: width 0.3s ease-in-out;
 `;
